e6a2749b46f2fe646784e6a34cfba175c32243ba
Fabian Vecchionacce <fabianevt@gmail.com>
2013-01-14 18:46:55 -0430

    Finalizado páginas estáticas.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,16 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
 require 'rspec/autorun'
+
+module ::RSpec::Core
+class ExampleGroup
+include Capybara::DSL
+include Capybara::RSpecMatchers
+end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
